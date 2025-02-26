dfba9f638405ff238995e9617f897293e2038274
Yosef Mets <maxmets@gmail.com>
2025-02-26 15:55:45 +0100

    HubKV test

--- server/api/db/index.put.js
+++ server/api/db/index.put.js
@@ -1,6 +1,14 @@
 export default eventHandler(async (event) => {
-  const db = await readBody(event)
-  await hubKV().set('db', db);
+  const db = await readBody(event);
+
+  console.log( db );
+
+  try {
+    await hubKV().set('db', db);
+  } catch (e) {
+    console.log(e);
+    return e
+  }
 
   return db
 })
