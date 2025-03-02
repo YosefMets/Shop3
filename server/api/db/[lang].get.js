9d4b6d2f663b0a2f8b9972ef344a25b376241d64
Yosef Mets <maxmets@gmail.com>
2025-03-02 14:08:22 +0100

    HubKV test

--- server/api/db/[lang].get.js
+++ server/api/db/[lang].get.js
@@ -1,5 +1,5 @@
 export default eventHandler(async (event) => {
   const { lang } = event.context.params || {}
-  // event.headers.append('content-type', 'json');
+  event.headers.append('content-type', 'text');
   return hubKV().get(`db:${lang}`);
 })
