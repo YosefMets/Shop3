64634b1a45d6a5b0dc1d33d523117ef06efea055
Yosef Mets <maxmets@gmail.com>
2025-03-04 15:15:23 +0100

    HubKV test

--- app/app.vue
+++ app/app.vue
@@ -2,24 +2,9 @@
 useSeoMeta({
   title: 'NuxtHub Starter',
   description: 'A Nuxt template to build your full-stack application on the edge.'
-})
-
-const db = ref(null);
-
-
-  const t = Date.now()
-  db.value = await $fetch('/api/db/en');
-  console.log( 'Fetch DB from KV:', Date.now() - t, db.value );
-
-
-const nestId = generateNestId( 35 );
+});
 </script>
 
 <template>
   <NuxtPage />
-  <hr>
-  {{ nestId }}
-  <hr>
-  {{ db ? JSON.parse( db ) : null }}
-  <hr>
 </template>
